ae95163a12227f5f6add1c9f36c1ba40dd590505
Sena Akbulut <Senakbulut@users.noreply.github.com>
2023-01-07 15:21:09 +0300

    backend completed

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -1,8 +1,8 @@
-import Post from "../models/Post";
-import User from "../models/User";
+import Post from "../models/Post.js";
+import User from "../models/User.js";
 
 /* Create */
-export const createPst = async (res, req) => {
+export const createPost = async (res, req) => {
   try {
     const { userId, description, picturePath } = req.body;
     const user = User.findById(userId);
@@ -24,3 +24,47 @@ export const createPst = async (res, req) => {
     res.status(409).json({ message: err.message });
   }
 };
+
+/* Read */
+
+export const getFeedPosts = async (res, req) =>{
+  try {
+    const post = await Post.find();
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({message: err.message});
+  }
+};
+
+export const getUserPosts = async (res, req) =>{
+  try {
+    const {userId} = req.params;
+    const post = await Post.find({userId});
+    res.status(200).json(post);
+  } catch (err) {
+    res.status(404).json({message: err.message});
+  }
+};
+
+/* Update */
+export const likePost = async (req, res) =>{
+  try {
+    const {id} = req.params;
+    const {userId} = req.body;
+    const post = await Post.findById(id);
+    const isLiked = post.likes.get(userId);
+    if(isLiked) {
+      post.likes.delete(userId);
+    }else{
+      post.likes.set(userId, true);
+    }
+    const updatedPost = await Post.findByIdAndUpdate(
+      id,
+      {likes: post.likes},
+      {new: true}
+    );
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    res.status(404).json({message: err.message});
+  }
+}
--- server/index.js
+++ server/index.js
@@ -14,6 +14,9 @@ import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
 import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import {users, posts} from "./data/index.js";
 
 /* Cofigurations */
 const __filename = fileURLToPath(import.meta.url);
@@ -58,5 +61,9 @@ mongoose
   })
   .then(() => {
     app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+
+    /* Add data one time */
+    // User.insertMany(users);
+    // Post.insertMany(posts);
   })
   .catch((error) => console.log(`${error} did not connect`));
