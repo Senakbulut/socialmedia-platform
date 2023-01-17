1e7db61670d278d33d79916c34b992a293ee1b0e
Sena Akbulut <Senakbulut@users.noreply.github.com>
2023-01-17 18:31:14 +0300

    theme definitions

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,29 @@
-import {BrowserRouter, Navigate, Routes, Route} from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
 import HomePage from "./routes/homePage";
 import LoginPage from "./routes/loginPage";
 import ProfilePage from "./routes/profilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import {createTheme} from "@mui/material/styles";
+import { themeSettings } from "./theme";
 
 function App() {
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
     <div className="app">
-     <BrowserRouter>
-     <Routes>
-      <Route path="/" element={<LoginPage />}/>
-      <Route path="/home" element={<HomePage />}/>
-      <Route path="/profile/:userId" element={<ProfilePage />}/>
-     </Routes>
-     </BrowserRouter>
+      <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+        </Routes>
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 }
