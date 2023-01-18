81b79778414544d306e7e5e7b3ef4c367f42853b
Sena Akbulut <Senakbulut@users.noreply.github.com>
2023-01-18 16:31:09 +0300

    navbar

--- client/src/routes/navbar/index.jsx
+++ client/src/routes/navbar/index.jsx
@@ -12,35 +12,119 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import {
-    Search,
-    Message,
-    DarkMode,
-    LightMode,
-    Notifications,
-    Help,
-    Menu,
-    Close,
+  Search,
+  Message,
+  DarkMode,
+  LightMode,
+  Notifications,
+  Help,
+  Menu,
+  Close,
 } from "@mui/icons-material";
-import {useDispatch, useSelector} from "react-redux";
-import {setMode, setLogout} from "state";
-import {useNavigate} from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { setMode, setLogout } from "state";
+import { useNavigate } from "react-router-dom";
 import FlexBetween from "../../components/FlexBetween";
 
 const Navbar = () => {
-    const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
-     const dispatch = useDispatch();
-     const navigate = useNavigate();
-     const user = useSelector((state)=>state.user);
-     const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
+  const [isMobileMenuToggled, setIsMobileMenuToggled] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const user = useSelector((state) => state.user);
+  const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
 
-     const theme = useTheme();
-     const neutralLight = theme.palette.neutral.light;
-     const dark = theme.palette.neutral.dark;
-     const background = theme.palette.background.default;
-     const primaryLight = theme.palette.primary.light;
-     const alt = theme.palette.background.alt;
+  const theme = useTheme();
+  const neutralLight = theme.palette.neutral.light;
+  const dark = theme.palette.neutral.dark;
+  const background = theme.palette.background.default;
+  const primaryLight = theme.palette.primary.light;
+  const alt = theme.palette.background.alt;
 
-     const fullName = `${user.firstName} ${user.lastName}`;
-  return <></>;
+  const fullName = `${user.firstName} ${user.lastName}`;
+  return (
+    <FlexBetween padding="1rem 6%" backgroundColor={alt}>
+      <FlexBetween gap="1.75rem">
+        <Typography
+          fontWeight="bold"
+          fontSize="clamp(1rem, 2rem, 2.25rem)"
+          color="primary"
+          onClick={() => navigate("/home")}
+          sx={{
+            "&:hover": {
+              color: primaryLight,
+              cursor: "pointer",
+            },
+          }}
+        >
+          Sociopedia
+        </Typography>
+        {isNonMobileScreens && (
+          <FlexBetween
+            backgroundColor={neutralLight}
+            borderRadius="9px"
+            gap="3rem"
+            padding="0.1rem 1.5rem"
+          >
+            <InputBase placeholder="Search..." />
+            <IconButton>
+              <Search />
+            </IconButton>
+          </FlexBetween>
+        )}
+      </FlexBetween>
+      {/*Desktop Nav */}
+      {isNonMobileScreens ? (
+        <FlexBetween gap="2rem">
+          <IconButton onClick={() => dispatch(setMode())}>
+            {theme.palette.mode === "dark" ? (
+              <DarkMode sx={{ fontSize: "25px" }} />
+            ) : (
+              <LightMode sx={{ color: dark, fontSize: "25px" }} />
+            )}
+          </IconButton>
+          <Message sx={{ fontSize: "25px" }} />
+          <Notifications sx={{ fontSize: "25px" }} />
+          <Help sx={{ fontSize: "25px" }} />
+          <FormControl sx={{ fontSize: "25px" }} value={fullName}>
+            <Select
+              value={fullName}
+              sx={{
+                backgroundColor: neutralLight,
+                width: 150,
+                borderRadius: "0.25rem",
+                p: "0.25rem 1rem",
+                "& .MuiSvgIcon-root": {
+                  pr: "0.25rem",
+                  width: "3rem",
+                },
+                "& .MuiSelect-select:focus": {
+                  backgroundColor: neutralLight,
+                },
+              }}
+              input={<InputBase />}
+            >
+              <MenuItem value={fullName}>
+                <Typography>{fullName}</Typography>
+              </MenuItem>
+              <MenuItem onClick={() => dispatch(setLogout())}>
+                {" "}
+                Log Out{" "}
+              </MenuItem>
+            </Select>
+          </FormControl>
+        </FlexBetween>
+      ) : (
+        <IconButton onClick={()=> setIsMobileMenuToggled(!isMobileMenuToggled)}>
+          <Menu />
+        </IconButton>
+      )}
+      {/*Mobile Nav */}
+        {!isNonMobileScreens && isMobileMenuToggled && (
+          <Box>
+
+          </Box>
+        )}
+    </FlexBetween>
+  );
 };
 export default Navbar;
